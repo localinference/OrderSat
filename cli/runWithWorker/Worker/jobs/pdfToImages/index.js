ce18b6601f4df6c959f29a3dfa81bc1a437f654d
Jori Lethinen <lehtinenjori03@gmail.com>
2026-03-17 19:00:05 +0200

    chore: scripts slowly coming together

--- cli/runWithWorker/Worker/jobs/pdfToImages/index.js
+++ cli/runWithWorker/Worker/jobs/pdfToImages/index.js
@@ -1,7 +1,12 @@
 import { PDFParse } from 'pdf-parse'
 
+const standardFontDataUrl = new URL(
+  '../../../../../node_modules/pdf-parse/node_modules/pdfjs-dist/standard_fonts/',
+  import.meta.url
+).href
+
 export async function pdfToImages({ pdfBuffer }) {
-  const parser = new PDFParse(pdfBuffer)
+  const parser = new PDFParse({ data: pdfBuffer, standardFontDataUrl })
   try {
     const result = await parser.getScreenshot({ scale: 1.5 })
     return result.pages
