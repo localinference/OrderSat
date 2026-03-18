ad06bc72f08d754b37d3d4e30b72738e4a3e6ffb
Jori Lethinen <lehtinenjori03@gmail.com>
2026-03-18 11:43:44 +0200

    chore: Working on scripts. And output annotation.

---
+++ cli/02_process_training_samples/index.js
@@ -0,0 +1,16 @@
+const t0 = performance.now()
+
+const sampleBase = './src/02_training_samples'
+
+const inputSamplePath = sampleBase + '/inputs'
+const outputSamplePath = sampleBase + '/outputs'
+
+const tokenizerBase = './src/03_tokenizers'
+
+try {
+  /*************************************************/
+  const { languages } = getArgs()
+  for (const language of languages) {
+  }
+  /*************************************************/
+} catch (err) {}
