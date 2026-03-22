501222a2cfe4cce49b65aecc17e2ca012c9c525c
Jori Lethinen <lehtinenjori03@gmail.com>
2026-03-22 14:22:57 +0200

    chore: Starting data work.

---
+++ cli/listUnannotated/index.js
@@ -0,0 +1,6 @@
+/**
+ * 1. lists ./src/02_training_samples/outputs/${langauge} dir
+ * 2. lists ./src/02_training_samples/inputs/${langauge} dir
+ * 3. diffs inputs without outputs to temp-unannotated at cwd root
+ * 4. annotators start annotating according to annotation skill (manually) not relevant for script and delete annotated from temp un annotated when finnished
+ */
